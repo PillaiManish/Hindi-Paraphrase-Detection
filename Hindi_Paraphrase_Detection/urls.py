09666c2c3b24dfeeb811026d900b03334b9d6f3f
PillaiManish <pillaimanish00@gmail.com>
2022-04-09 09:41:42 +0530

    Added Page & connected with temp URL

--- Hindi_Paraphrase_Detection/urls.py
+++ Hindi_Paraphrase_Detection/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from DetectionApp.views import getIndex
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('as/', getIndex),
 ]
