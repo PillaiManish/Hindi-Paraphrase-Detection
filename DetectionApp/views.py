4ffbf14f226a8fcc25dee3d8467072be8eca0f94
PillaiManish <pillaimanish00@gmail.com>
2022-04-09 11:04:02 +0530

    Importing intlk & setup done

--- DetectionApp/views.py
+++ DetectionApp/views.py
@@ -1,5 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from inltk.inltk import setup
+setup('hi')
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    if request.method == "GET":
+        return render(request, 'index.html')
+    else:
+        phrase1 = request.POST["phrase1"];
+        phrase2 = request.POST["phrase2"];
+        return HttpResponse(phrase1);
