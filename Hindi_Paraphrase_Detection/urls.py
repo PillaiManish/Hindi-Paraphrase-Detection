b361a96fa887d6a8bb18ddf2b52357f027d2d3da
PillaiManish <pillaimanish00@gmail.com>
2022-04-09 10:00:32 +0530

    View function name changed

--- Hindi_Paraphrase_Detection/urls.py
+++ Hindi_Paraphrase_Detection/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from DetectionApp.views import getIndex
+from DetectionApp.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('as/', getIndex),
+    path('as/', index),
 ]
