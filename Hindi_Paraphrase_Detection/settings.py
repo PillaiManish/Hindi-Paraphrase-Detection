4ff0e001fd1fa06bd7223197602b26bf2bc9cc79
PillaiManish <pillaimanish00@gmail.com>
2022-04-14 07:48:35 +0530

    Major updates

--- Hindi_Paraphrase_Detection/settings.py
+++ Hindi_Paraphrase_Detection/settings.py
@@ -130,3 +130,20 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+import os
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+}
