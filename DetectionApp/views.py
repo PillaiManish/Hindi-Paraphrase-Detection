b317d21be6410824b1880cc3a3384efe9b71d7db
PillaiManish <pillaimanish00@gmail.com>
2022-04-20 20:13:36 +0530

    Added Slider

--- DetectionApp/views.py
+++ DetectionApp/views.py
@@ -1,25 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from inltk.inltk import setup
+setup('hi')
+from inltk.inltk import get_sentence_similarity
 
 # Create your views here.
 def index(request):
     if request.method == "GET":
-        return render(request, 'index.html')
+        session = {}
+        session['display'] = False
+        session['value'] = 0
+        return render(request, 'index.html', session)
 
     elif request.method == "POST":
-        from inltk.inltk import setup
-        setup('hi')
-        print("YESS 1")
-        # from inltk.inltk import get_sentence_similarity
-        print("YESS 2")
+        phrase1 = request.POST["phrase1"];
+        phrase2 = request.POST["phrase2"];
+        similarlity = get_sentence_similarity(phrase1, phrase2, 'hi')
 
-        phrase1 = "भेजना चाहते हैं हिंदी में मैसेज लेकिन नहीं आती टाइपिंग |"
-        phrase2 = "भेजना चाहते हैं हिंदी में मैसेज लेकिन नहीं आती टाइपिंग |"
-        # similarlity = get_sentence_similarity(phrase1, phrase2, 'hi')
-        from inltk.inltk import get_similar_sentences
-        get_similar_sentences(phrase1, 1, 'hi', degree_of_aug = 0.1)
-
-        print("YESS 3")
-
-        # return HttpResponse(similarlity);
+        session = {}
+        session['display'] = True
+        session['value'] = similarlity
+        return render(request, 'index.html', session);
